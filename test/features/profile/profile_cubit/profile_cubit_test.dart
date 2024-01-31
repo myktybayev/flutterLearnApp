863d49c298a11729c8b745925c18267eb4b906e5
Alnur Rysbek <100896741+aleke88884@users.noreply.github.com>
2024-01-31 13:03:53 +0600

    fixed

--- test/features/profile/profile_cubit/profile_cubit_test.dart
+++ test/features/profile/profile_cubit/profile_cubit_test.dart
@@ -4,34 +4,29 @@ import 'package:flutter_learn_app/features/profile/ui/cubit/profile/profile_stat
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group(
-    'ProfileCubit',
-    () {
-      late ProfileCubit profileCubit;
-      setUp(() {
-        profileCubit = ProfileCubit();
-      });
+  late ProfileCubit profileCubit;
+  setUp(() {
+    profileCubit = ProfileCubit();
+  });
 
-      blocTest<ProfileCubit, ProfileState>(
-        'emits updated state with new name and phone number',
-        build: () => profileCubit,
-        act: (profileCubit) => profileCubit.saveData('John Doe', '87711593543'),
-        expect: () => [
-          const ProfileState(
-              name: 'John Doe', phoneNumber: '87711593543', isEdited: true),
-        ],
-      );
-
-      blocTest<ProfileCubit, ProfileState>(
-        'emits state with isEdited: false when restartEdit is called',
-        build: () => profileCubit,
-        act: (profileCubit) => profileCubit.restartEdit(),
-        expect: () => [
-          const ProfileState(isEdited: false),
-        ],
-      );
-
-      tearDown(() => profileCubit.close());
-    },
+  blocTest<ProfileCubit, ProfileState>(
+    'emits updated state with new name and phone number',
+    build: () => profileCubit,
+    act: (profileCubit) => profileCubit.saveData('John Doe', '87711593543'),
+    expect: () => [
+      const ProfileState(
+          name: 'John Doe', phoneNumber: '87711593543', isEdited: true),
+    ],
   );
+
+  blocTest<ProfileCubit, ProfileState>(
+    'emits state with isEdited: false when restartEdit is called',
+    build: () => profileCubit,
+    act: (profileCubit) => profileCubit.restartEdit(),
+    expect: () => [
+      const ProfileState(isEdited: false),
+    ],
+  );
+
+  tearDown(() => profileCubit.close());
 }
